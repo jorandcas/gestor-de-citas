43e3d5060b187098d5d92ddeb4eeb48c4ea3f627
Jorandcas <jorgeand.jc@gmail.com>
2026-02-09 17:27:15 -0600

    Fix: Add production config for database connection

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -24,15 +24,17 @@ const config = {
   //     bigNumberStrings: true,
   //   },
   // },
-  // production: {
-  //   username: process.env.PROD_DB_USERNAME,
-  //   password: process.env.PROD_DB_PASSWORD,
-  //   database: process.env.PROD_DB_NAME,
-  //   host: process.env.PROD_DB_HOSTNAME,
-  //   port: process.env.PROD_DB_PORT,
-  //   dialect: 'mysql',
-  //   // dialectOptions: {
-  // },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
 };
 
 export default config;
